76000b53b8e3c40c921017c01469abbfeaf93a39
Stephen Paul Weber <singpolyma@singpolyma.net>
2019-07-21 21:35:34 -0500

    Map types are generic

--- lib/dhall/types.rb
+++ lib/dhall/types.rb
@@ -4,12 +4,16 @@ require "dhall/builtins"
 
 module Dhall
 	module Types
-		MAP_ENTRY = RecordType.new(
-			record: {
-				"mapKey" => Builtins[:Text], "mapValue" => Builtins[:Text]
-			}
-		)
+		def self.MAP_ENTRY(k: Builtins[:Text], v: Builtins[:Text])
+			RecordType.new(
+				record: {
+					"mapKey" => k, "mapValue" => v
+				}
+			)
+		end
 
-		MAP = Builtins[:List].call(MAP_ENTRY)
+		def self.MAP(k: Builtins[:Text], v: Builtins[:Text])
+			Builtins[:List].call(MAP_ENTRY(k: k, v: v))
+		end
 	end
 end
